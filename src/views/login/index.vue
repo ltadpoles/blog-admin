5f2501ec07c198bf27e0b86bfbfb922b426bfb95
tadpole <ltadpole@163.com>
2021-02-28 23:56:20 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,30 +1,50 @@
 <template>
-    <a-form
-        name="custom-validation"
-        ref="formRef"
-        :model="formState"
-        :rules="rules"
-        @finish="handleFinish"
-        @finishFailed="handleFinishFailed"
-    >
-        <a-form-item has-feedback name="user">
-            <a-input v-model:value="formState.pass" autocomplete="off">
-                <template #prefix>
-                    <UserOutlined />
-                </template>
-            </a-input>
-        </a-form-item>
-        <a-form-item has-feedback name="passWord">
-            <a-input v-model:value="formState.checkPass" type="password" autocomplete="off">
-                <template #prefix>
-                    <LockOutlined />
-                </template>
-            </a-input>
-        </a-form-item>
-        <a-form-item>
-            <a-button block type="primary" html-type="submit">登录</a-button>
-        </a-form-item>
-    </a-form>
+    <div class="login">
+        <div class="login-content">
+            <div class="login-title">
+                <img class="title-logo" src="@/assets/imgs/logo.svg" alt="" />
+                <span>Blog Admin</span>
+            </div>
+            <a-form
+                name="custom-validation"
+                class="login-form"
+                ref="formRef"
+                :model="formState"
+                :rules="rules"
+                v-bind="layout"
+                @finish="handleFinish"
+                @finishFailed="handleFinishFailed"
+            >
+                <a-form-item has-feedback name="userName">
+                    <a-input
+                        v-model:value="formState.userName"
+                        autocomplete="off"
+                        placeholder="账号"
+                    >
+                        <template #prefix>
+                            <UserOutlined />
+                        </template>
+                    </a-input>
+                </a-form-item>
+                <a-form-item has-feedback name="passWord">
+                    <a-input
+                        v-model:value="formState.passWord"
+                        type="password"
+                        autocomplete="off"
+                        placeholder="密码"
+                        visibilityToggle
+                    >
+                        <template #prefix>
+                            <LockOutlined />
+                        </template>
+                    </a-input>
+                </a-form-item>
+                <a-form-item>
+                    <a-button block type="primary" html-type="submit">登录</a-button>
+                </a-form-item>
+            </a-form>
+        </div>
+    </div>
 </template>
 <script>
 import { defineComponent, reactive, ref } from 'vue'
@@ -38,37 +58,44 @@ export default defineComponent({
             passWord: ''
         })
 
-        let validateUser = async (rule, value) => {
-            if (value === '') {
-                return Promise.reject('请输入用户名')
-            } else {
-                return Promise.resolve()
-            }
-        }
+        // let validateUser = async (rule, value) => {
+        //     if (value === '') {
+        //         return Promise.reject('请输入用户名')
+        //     } else {
+        //         return Promise.resolve()
+        //     }
+        // }
 
-        let validatePass = async (rule, value) => {
-            if (value === '') {
-                return Promise.reject('请输入密码')
-            } else {
-                if (formState.checkPass !== '') {
-                    formRef.value.validateField('checkPass')
-                }
-                return Promise.resolve()
-            }
-        }
+        // let validatePass = async (rule, value) => {
+        //     if (value === '') {
+        //         return Promise.reject('请输入密码')
+        //     } else {
+        //         return Promise.resolve()
+        //     }
+        // }
 
         const rules = {
-            user: [
+            userName: [
                 {
-                    validator: validateUser,
-                    trigger: 'change'
+                    required: true,
+                    message: '请输入用户名',
+                    trigger: 'blur'
                 }
+                // {
+                //     validator: validateUser,
+                //     trigger: 'change'
+                // }
             ],
             passWord: [
                 {
-                    validator: validatePass,
-                    trigger: 'change'
+                    required: true,
+                    message: '请输入密码',
+                    trigger: 'blur'
                 }
+                // {
+                //     validator: validatePass,
+                //     trigger: 'change'
+                // }
             ]
         }
 
@@ -84,13 +111,20 @@ export default defineComponent({
             formRef.value.resetFields()
         }
 
+        const layout = {
+            wrapperCol: {
+                span: 24
+            }
+        }
+
         return {
             formState,
             formRef,
             rules,
             handleFinishFailed,
             handleFinish,
-            resetForm
+            resetForm,
+            layout
         }
     },
     components: {
@@ -99,3 +133,29 @@ export default defineComponent({
     }
 })
 </script>
+
+<style lang="less" scoped>
+.login {
+    height: 100vh;
+    background: url('../../assets/imgs/login.svg') no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    .login-content {
+        width: 400px;
+        margin-top: 20px;
+        .login-title {
+            display: flex;
+            justify-content: center;
+            height: 100px;
+            line-height: 100px;
+            font-size: 24px;
+            font-weight: 700;
+            .title-logo {
+                width: 40px;
+                margin-right: 15px;
+            }
+        }
+    }
+}
+</style>
