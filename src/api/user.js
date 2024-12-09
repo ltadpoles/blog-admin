2051be5649b165b5e9b566b94abed247fcbf3fca
tadpole <ltadpole@163.com>
2024-12-09 18:09:36 +0800

    http

--- src/api/user.js
+++ src/api/user.js
@@ -7,5 +7,5 @@ import { CONFIG } from '../config'
 
 // 用户登录
 export const loginIn = (data) => {
-    return http.post(CONFIG.HTTP_BASEURL + '/login', data)
+  return http.post(CONFIG.HTTP_BASEURL + '/login', data)
 }
