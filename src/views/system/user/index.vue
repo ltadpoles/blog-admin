71a6aa1d865dac3bb16946c82e8f55d9552e7981
tadpole <ltadpole@163.com>
2025-07-04 14:03:45 +0800

    feat: user

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,8 +1,124 @@
 <template>
-  <div>用户管理</div>
+  <div class="view-base">
+    <div class="view-base-form">
+      <el-form ref="formRef" :inline="true" :model="formData">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="formData.username" placeholder="请输入用户名" clearable />
+        </el-form-item>
+        <el-form-item label="昵称" prop="name">
+          <el-input v-model="formData.name" placeholder="请输入昵称" clearable />
+        </el-form-item>
+        <el-form-item label="状态" prop="status">
+          <el-select v-model="formData.status" placeholder="请选择状态" clearable>
+            <el-option label="全部" value="" />
+            <el-option label="启用" value="1" />
+            <el-option label="禁用" value="2" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div class="view-base-form-btn">
+        <div class="view-base-form-btn-left"></div>
+        <div class="view-base-form-btn-right">
+          <el-button icon="Refresh" @click="reset(formRef)">重置</el-button>
+          <el-button type="primary" icon="Search" @click="search">搜索</el-button>
+        </div>
+      </div>
+    </div>
+
+    <div class="view-base-table">
+      <el-table :data="tableData" border>
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="avatar" label="头像" align="center">
+          <template #default="scope">
+            <img class="list-avatar" :src="scope.row.avatar" alt="" />
+          </template>
+        </el-table-column>
+        <el-table-column prop="username" label="用户名" />
+        <el-table-column prop="name" label="昵称" />
+        <el-table-column prop="phone" label="手机号" />
+        <el-table-column prop="email" label="邮箱" />
+        <el-table-column prop="sex" label="性别">
+          <template #default="scope">
+            {{ scope.row.sex === 1 ? '男' : '女' }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="status" label="状态" />
+        <el-table-column prop="createTime" label="注册时间">
+          <template #default="scope">
+            {{ dayjs(scope.row.createTime).format('YYYY-MM-DD HH:mm') }}
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <div class="view-base-pagination">
+      <el-pagination
+        v-model:current-page="pageQuery.currentPage"
+        v-model:page-size="query.pageSize"
+        size="small"
+        layout="total, prev, pager, next"
+        :total="pageQuery.total"
+        @current-change="currentChange"
+      />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ElMessage } from 'element-plus'
+import { reactive, ref, useTemplateRef, onMounted } from 'vue'
+import { dayjs } from 'element-plus'
+
+const formRef = useTemplateRef('formRef')
+const formData = reactive({
+  name: '',
+  username: '',
+  status: ''
+})
+
+const tableData = ref([
+  {
+    avatar: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+  }
+])
+const query = reactive({
+  pageNum: 1,
+  pageSize: 10,
+  param: {}
+})
+const pageQuery = reactive({
+  total: 0,
+  currentPage: 1
+})
+
+const getList = () => {
+  ElMessage.error('获取数据失败')
+}
+const currentChange = page => {
+  query.pageNum = page
+  getList(query)
+}
+
+const search = () => {
+  pageQuery.currentPage = 1
+  query.pageNum = 1
+  query.param = Object.assign(query.param, formData)
+  getList(query)
+}
+const reset = formEl => {
+  if (!formEl) {
+    return
+  }
+  formEl.resetFields()
+  pageQuery.currentPage = 1
+  query.param = {}
+  getList(query)
+}
+
+onMounted(() => {
+  getList(query)
+})
+</script>
 
 <style lang="scss" scoped>
 @use './index.scss' as *;
