b379c88e420c2f735c18c6d4e19029b477eccc16
tadpole <ltadpole@163.com>
2025-07-10 20:10:04 +0800

    feat: messages

--- src/views/article/messages/index.vue
+++ src/views/article/messages/index.vue
@@ -1,8 +1,149 @@
 <template>
-  <div>留言管理</div>
+  <div class="view-base">
+    <div class="view-base-form">
+      <el-form ref="formRef" :inline="true" :model="formData">
+        <el-form-item label="留言信息" prop="name">
+          <el-input v-model="formData.name" placeholder="请输入留言信息" clearable />
+        </el-form-item>
+        <el-form-item label="留言时间" prop="date">
+          <el-date-picker
+            v-model="formData.date"
+            type="daterange"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          />
+        </el-form-item>
+      </el-form>
+      <div class="view-base-form-btn">
+        <div class="view-base-form-btn-left">
+          <el-button icon="Delete" type="danger" @click="delData">删除</el-button>
+        </div>
+        <div class="view-base-form-btn-right">
+          <el-button icon="Refresh" @click="reset(formRef)">重置</el-button>
+          <el-button type="primary" icon="Search" @click="search">搜索</el-button>
+        </div>
+      </div>
+    </div>
+
+    <div class="view-base-table">
+      <el-table :data="tableData" border @selection-change="selectionChange">
+        <el-table-column type="selection" width="45" />
+        <el-table-column prop="description" label="操作人" />
+        <el-table-column prop="description" label="联系方式" />
+        <el-table-column prop="description" label="关联文章" />
+        <el-table-column prop="createTime" label="留言时间">
+          <template #default="scope">
+            {{ dayjs(scope.row.createTime).format('YYYY-MM-DD HH:mm') }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="name" label="留言内容" />
+        <el-table-column label="操作" width="100" align="center">
+          <template #default="scope">
+            <el-popconfirm
+              confirm-button-text="确认"
+              cancel-button-text="取消"
+              icon="InfoFilled"
+              title="确认删除?"
+              @confirm="delConfirm(scope.row)"
+            >
+              <template #reference>
+                <el-button link type="danger" icon="Delete" />
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <div class="view-base-pagination">
+      <el-pagination
+        v-model:current-page="pageQuery.currentPage"
+        v-model:page-size="query.pageSize"
+        size="small"
+        layout="total, prev, pager, next"
+        :total="pageQuery.total"
+        @current-change="currentChange"
+      />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ElMessage } from 'element-plus'
+import { reactive, ref, useTemplateRef, onMounted } from 'vue'
+import { dayjs } from 'element-plus'
+import { ElMessageBox } from 'element-plus'
+
+const formRef = useTemplateRef('formRef')
+const formData = reactive({
+  name: '',
+  status: '',
+  date: ''
+})
+
+const tableData = ref([])
+const query = reactive({
+  pageNum: 1,
+  pageSize: 10,
+  param: {}
+})
+const pageQuery = reactive({
+  total: 0,
+  currentPage: 1
+})
+
+const getList = () => {
+  ElMessage.error('获取数据失败')
+}
+const currentChange = page => {
+  query.pageNum = page
+  getList(query)
+}
+
+const search = () => {
+  pageQuery.currentPage = 1
+  query.pageNum = 1
+  query.param = Object.assign(query.param, formData)
+  getList(query)
+}
+const reset = formEl => {
+  if (!formEl) {
+    return
+  }
+  formEl.resetFields()
+  pageQuery.currentPage = 1
+  query.param = {}
+  getList(query)
+}
+
+const delData = () => {
+  if (multipleSelection.value.length === 0) {
+    return ElMessage.error('请至少选择一条数据')
+  }
+
+  ElMessageBox.confirm('确认删除选中留言？', '提示', {
+    confirmButtonText: '确认',
+    cancelButtonText: '取消',
+    type: 'warning'
+  }).then(async () => {
+    ElMessage.success('删除成功')
+    getList(query)
+  })
+}
+
+const delConfirm = () => {
+  ElMessage.success('删除成功')
+}
+
+const multipleSelection = ref([])
+const selectionChange = val => {
+  multipleSelection.value = val
+}
+
+onMounted(() => {
+  getList(query)
+})
+</script>
 
 <style lang="scss" scoped>
 @use './index.scss' as *;
